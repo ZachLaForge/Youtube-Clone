d326a63f6009e50c9f57242797376706574d28d0
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-04-14 12:11:51 -0500

    added the GET, POST, and DELETE by ID method to the comment app's view.py

--- backend/drf_jwt_backend/comment/views.py
+++ backend/drf_jwt_backend/comment/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -29,4 +30,18 @@ def user_comment(request):
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def c_detail(request, pk):
+    comment = get_object_or_404(Comment, pk = pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data = request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
