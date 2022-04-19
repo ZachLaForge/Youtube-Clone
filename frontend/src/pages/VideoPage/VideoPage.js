c9e51cf36183f6d577bb26b1c05ecc262fd8b26d
Zachary LaForge <zach.laforge@hotmail.com>
2022-04-19 14:35:44 -0400

    added Title and comment box for SpaceX video

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -9,9 +9,11 @@ const VideoPage = (props) => {
 
     return ( 
     <div className="center-video">
+        <h1>Why Falcon 9 is Better than Even SpaceX Thought</h1>
         <div>
         <ReactPlayer url="https://www.youtube.com/watch?v=8iQMTrC-spc"  />
         </div>
+            <p className="border-entry">Falcon 9 has seemingly overachieved on its promises of rapid reuse with little to no refurbishment between flights. Ian breaks down data from SpaceX's workhorse showing how Falcon 9 Block 5 surpassed even SpaceX's expectations.</p>
         <div>
             <small>
                 <AddComment />    
