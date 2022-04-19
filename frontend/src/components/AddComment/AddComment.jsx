f40cad3c23c49b3b50f5d7f9f72988b3fb769a34
itachi363 <itachi363@yahoo.com>
2022-04-19 15:30:54 -0400

    added auth token to post but still giving unathorized

--- frontend/src/components/AddComment/AddComment.jsx
+++ frontend/src/components/AddComment/AddComment.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import axios from 'axios'
+import useAuth from "../../hooks/useAuth";
 
 
 const AddComment = (props) => {
 
-    const [user, setUser] = useState('')
+    const [user, token] = useAuth();
+
+    const [users, setUser] = useState('')
     const [video_id, setVideo_Id] = useState('')
     const [text, setText] = useState('')
     const [likes, setLikes] = useState('')
@@ -14,7 +17,10 @@ const AddComment = (props) => {
         event.preventDefault()
         try {
             const response = await axios.post('http://127.0.0.1:8000/comment/auth/', {
-                user: user,
+                headers: {
+                    Authorization: "Bearer " + token,
+                  },
+                user: users,
                 video_id: video_id,
                 text: text,
                 likes: likes,
@@ -38,7 +44,7 @@ const AddComment = (props) => {
             </div>
             <div className="border-bar">
                 <label className="spacing">User</label>
-                <input type="number" value={user} onChange={(event) => setUser(event.target.value)} />
+                <input type="number" value={users} onChange={(event) => setUser(event.target.value)} />
                 <label className="spacing">Video_id:</label>
                 <input type="text" value={video_id} onChange={(event) => setVideo_Id(event.target.value)} />
                 <label className="spacing">Text:</label>
